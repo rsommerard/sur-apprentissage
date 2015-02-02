474314e6a6bb007f7e01f3125f368af51ab3e4c8
Romain SOMMERARD <rsommerard@gmail.com>
2015-02-03 00:01:52 +0100

    Add beginning of k-fold cross validation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# sur-apprentissage
+# Sur-apprentissage
+
+- Récupérer les données
+- Enrichir l’espace d’hypothèses avec les polynômes d’ordre m
+- Estimer le risque réel par validation croisée à K plis
+- Trouver le polynôme le plus adapté pour les moindres carrés
+- Vérifier si l’algorithme sur-apprend
+
--- main.py
+++ main.py
@@ -12,12 +12,17 @@ def load_data():
   y = numpy.loadtxt('data/y_C3.txt')
 
 
-def theta(a):
+def calc_theta(a):
   return numpy.dot(numpy.linalg.inv(numpy.dot(a, a.T)), numpy.dot(a, y))
 
 
 def f_theta(matrix, theta):
   return numpy.dot(theta.T, matrix)
+
+
+def j_theta(a, b, theta):
+  tmp = (b - numpy.dot(a.T, theta))
+  return ((1.0/N) * numpy.dot(tmp.T, tmp))
 
 
 def enrichissement(matrix, ordre):
@@ -47,14 +52,86 @@ def print_graphs():
   pyplot.show()
 
 
+def training_matrix(k):
+  tmpx = x
+  tmpy = y
+
+  rnd = numpy.arange(N)
+  numpy.random.shuffle(rnd)
+  print N/k
+  for i in range(0, (N/k)):
+    print rnd[i]
+    tmpx = numpy.delete(tmpx, rnd[i])
+    tmpy = numpy.delete(tmpy, rnd[i])
+
+  print len(tmpx)
+  print len(tmpy)
+  return tmpx, tmpy
+
+
 def main():
   load_data()
-  print "Matrice: \n", enrichissement(x, 10)
-  print "Theta: ", theta(enrichissement(x, 10))
-  print_graphs()
   
-  a = numpy.array([0, 1, 2, 3, 4, 5])
-  numpy.delete(a, 3)
+  K = 10
+
+  matrix = enrichissement(x, 1)
+  test = numpy.split(x, K)
+
+  print numpy.concatenate(test)
+  print len(numpy.concatenate(test))
+
+  """matrix = enrichissement(x, 1)
+  theta = calc_theta(matrix)
+  error = j_theta(matrix, y, theta)
+  print "Ordre: 1"
+  print "Theta: ", theta
+  print "Erreur: ", error
+
+  matrix = enrichissement(x, 10)
+  theta = calc_theta(matrix)
+  error = j_theta(matrix, y, theta)
+  print "Ordre: 10"
+  print "Theta: ", theta
+  print "Erreur: ", error
+
+  matrix = enrichissement(x, 20)
+  theta = calc_theta(matrix)
+  error = j_theta(matrix, y, theta)
+  print "Ordre: 20"
+  print "Theta: ", theta
+  print "Erreur: ", error
+
+  matrix = enrichissement(x, 23)
+  theta = calc_theta(matrix)
+  error = j_theta(matrix, y, theta)
+  print "Ordre: 23"
+  print "Theta: ", theta
+  print "Erreur: ", error
+
+  matrix = enrichissement(x, 25)
+  theta = calc_theta(matrix)
+  error = j_theta(matrix, y, theta)
+  print "Ordre: 25"
+  print "Theta: ", theta
+  print "Erreur: ", error"""
+
+  """K = 10
+  ordre = 1
+  diff = 1
+  while(diff > 0):
+    matrix = enrichissement(x, ordre)
+    for i in range(0, 10):
+      tx, ty = training_matrix(K)
+      theta = theta(training)
+      error = j_theta(training, y, theta)
+      diff = error - diff"""
+
+  #matrix = enrichissement(x, 1)
+  #theta = theta(matrix)
+
+  #print "Matrice: \n", enrichissement(x, 10)
+  #print "Theta: ", theta(enrichissement(x, 10))
+  #print_graphs()
 
 
 if __name__ == '__main__':
