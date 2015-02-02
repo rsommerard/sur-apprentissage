16349f99d3f38918eaaf4eeebabe7960d27d72e2
Romain SOMMERARD <rsommerard@gmail.com>
2015-02-02 14:01:57 +0100

    Poly ok

--- main.py
+++ main.py
@@ -4,22 +4,58 @@ import numpy
 from matplotlib import pyplot
 import random
 
+
 def load_data():
-  global t, p
-  t = numpy.loadtxt('data/x_C3.txt')
-  p = numpy.loadtxt('data/y_C3.txt')
+  global x, y, N
+  x = numpy.loadtxt('data/x_C3.txt')
+  N = len(x)
+  y = numpy.loadtxt('data/y_C3.txt')
+
+
+def theta(a):
+  return numpy.dot(numpy.linalg.inv(numpy.dot(a, a.T)), numpy.dot(a, y))
+
+
+def f_theta(matrix, theta):
+  return numpy.dot(theta.T, matrix)
+
+
+def enrichissement(matrix, ordre):
+  tmp = numpy.vstack((numpy.ones(N), matrix))
   
-def enrichissement(ligne, ordre):
-  matrix = numpy.vstack((numpy.ones((1, len(ligne))), ligne))
-  
-  for i in range(2, ordre):
-    matrix = numpy.vstack((matrix, numpy.power(ligne, i)))
+  if ordre == 0:
+    return numpy.ones(N)
     
-  return matrix
+  if ordre == 1:
+    return tmp
+  
+  for i in range(2, ordre+1):
+    tmp = numpy.vstack((tmp, numpy.power(matrix, i)))
+    
+  return tmp
+
+
+def print_graphs():
+  pyplot.figure(1)
+  pyplot.plot(x, y, '.')
+  pyplot.plot(x, f_theta(enrichissement(x, 10), theta(enrichissement(x, 10))), '.')
+  pyplot.legend()
+  pyplot.grid(True)
+  pyplot.ylabel('y')
+  pyplot.xlabel('x')
+
+  pyplot.show()
+
 
 def main():
   load_data()
-  print enrichissement(t, 3)
+  print "Matrice: \n", enrichissement(x, 10)
+  print "Theta: ", theta(enrichissement(x, 10))
+  print_graphs()
+  
+  a = numpy.array([0, 1, 2, 3, 4, 5])
+  numpy.delete(a, 3)
+
 
 if __name__ == '__main__':
   main()
